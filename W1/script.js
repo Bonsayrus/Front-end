4be3cec0435593df530ea83924cd3afec85e49a4
Bonsayrus <akek0620@gmail.com>
2026-02-23 20:25:24 +0200

    added work 1

---
+++ W1/script.js
@@ -0,0 +1,6 @@
+alert("Hello!");
+
+const userName = prompt("What is your name?");
+const country = prompt("What are you from?");
+
+alert("Nice to meet you, " + userName + "! You are from " + country + "!");

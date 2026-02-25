457ba2472a29194133c1ac8c66dbe195a73c4589
Bonsayrus <akek0620@gmail.com>
2026-02-25 22:33:08 +0200

    Work 3.1

---
+++ W3.3/script.js
@@ -0,0 +1,4 @@
+const number = prompt("Enter a five-digit number:");
+
+// console.log(number[0] + " " + number[1] + " " + number[2] + " " + number[3] + " " + number[4]);
+console.log(number.split("").join(" "));

7b0bee16d496c77910fc3ce8f1a0bdc2e1ebf57c
Bonsayrus <akek0620@gmail.com>
2026-02-25 22:09:04 +0200

    Work 3.2

---
+++ W3.2/script.js
@@ -0,0 +1,17 @@
+const number = 31;
+const string = "31";
+const boolean = true;
+const nuLL = null;
+const _undefined = undefined;
+const bigInt = 31n;
+const symbol = Symbol("31");
+const object = {31: "value"};
+
+console.log(typeof number);
+console.log(typeof string);
+console.log(typeof boolean);
+console.log(typeof nuLL);
+console.log(typeof _undefined);
+console.log(typeof bigInt);
+console.log(typeof symbol);
+console.log(typeof object);

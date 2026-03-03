92ec75989a72e88726accec2938b8777d98701f6
Bonsayrus <akek0620@gmail.com>
2026-03-03 19:29:33 +0200

    Work 7.1

---
+++ W7.1/script.js
@@ -0,0 +1,16 @@
+// Написати функцію, яка приймає 1 параметр. Та складае значення з тим, 
+// що передали перший раз і т. д. Все це із замиканнями
+
+function createSum() {
+    let total = 0;
+    return function(num) {
+        total += num;
+        return total; 
+    }
+}
+const sum = createSum();
+
+console.log(sum(4)); // 4
+console.log(sum(6)); // 10
+console.log(sum(10)); // 20
+console.log(sum(7)); // 27

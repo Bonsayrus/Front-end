26394cad7327873517618777db157ea8d95539df
Bonsayrus <akek0620@gmail.com>
2026-03-17 20:00:45 +0200

    Work 17.1

---
+++ W17.1/script.js
@@ -0,0 +1,23 @@
+// Створіть клас Calculator, який буде мати методи для виконання базових математичних операцій: додавання, віднімання, множення та ділення.
+
+class Calculator {
+ add(a, b){
+    return a + b;
+ }
+ subtract(a, b){
+    return a - b;
+ }
+ multiply(a, b){
+    return a * b;
+ }
+ divide(a, b){
+    if (b === 0) return "Ділення на нуль";
+    return a / b;
+ }
+}
+const calc = new Calculator();
+console.log(calc.add(5, 3)); 
+console.log(calc.subtract(10, 4)); 
+console.log(calc.multiply(3, 6)); 
+console.log(calc.divide(8, 0)); 
+
